45c12af04bb23e11a9567b4889b360575e4e3f42
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-22 15:00:38 +0100

    Push task 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// request data on actor and count number of films
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/people/18';
+
+request(url, { json: true }, (error, response, body) => {
+  if (response && response.statusCode === 200) {
+    console.log(body.films.length);
+  } else {
+    console.error(error, "hello");
+  }
+});
