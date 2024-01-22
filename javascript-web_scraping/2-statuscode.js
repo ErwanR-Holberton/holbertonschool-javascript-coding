94b42cfc29880ba278e7c13ef842120d061c7139
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-22 13:08:29 +0100

    Corrected semistandard

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 // send request and get status
-const fs = require('fs');
 const request = require('request');
 
 const url = process.argv[2];
 
 request(url, (error, response) => {
+  if (error) {
+    console.error(`Error making request: ${error.message}`);
+    return;
+  }
   console.log(`Code: ${response.statusCode}`);
 });
