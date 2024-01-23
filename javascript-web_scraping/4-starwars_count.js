7710779fcf1455e41714acb576ed41690b10040b
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-23 12:26:03 +0100

    Use argv 2 as request

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,12 +2,15 @@
 // request data on actor and count number of films
 const request = require('request');
 
-const url = 'https://swapi-api.hbtn.io/api/people/18';
+const url = process.argv[2];
 
 request(url, { json: true }, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    console.log(body.films.length);
+    const filmsWithWedge = body.results.filter((film) =>
+      film.characters.includes(`https://swapi-api.hbtn.io/api/people/${18}/`)
+    );
+    console.log(filmsWithWedge.length);
   }
 });
