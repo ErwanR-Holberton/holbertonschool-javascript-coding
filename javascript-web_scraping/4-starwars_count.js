bdbcccb67038949fab72e31efc595fc46ef825d8
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-23 12:04:18 +0100

    inverted condition

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,9 +5,9 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/people/18';
 
 request(url, { json: true }, (error, response, body) => {
-  if (response && response.statusCode === 200) {
-    console.log(body.films.length);
-  } else {
+  if (error) {
     console.error(error);
+  } else {
+    console.log(body.films.length);
   }
 });
