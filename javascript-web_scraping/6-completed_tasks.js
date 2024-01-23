0731e7f1e9627885e56ba47ff25df0a1975c6121
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-23 16:09:22 +0100

    Corrected output

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -6,13 +6,17 @@ const url = process.argv[2];
 
 request(url, { json: true }, (error, response, body) => {
   if (response && response.statusCode === 200) {
-    let count = 0;
+    let completedByUser = {};
     for (const task in body) {
       if (body[task].completed === true) {
-        count += 1;
+        if (!(body[task]['userId'] in completedByUser)) {
+            completedByUser[body[task]['userId']] = 1;
+        } else {
+            completedByUser[body[task]['userId']] += 1;
+        }
       }
     }
-    console.log(count);
+    console.log(completedByUser);
   } else {
     console.error(error, 'hello');
   }
