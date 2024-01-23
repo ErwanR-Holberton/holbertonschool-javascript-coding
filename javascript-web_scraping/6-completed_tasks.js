de4ceee0a4d1261bf884b7d3ce7516602121b700
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-23 18:36:02 +0100

    try formating manualy

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -16,7 +16,19 @@ request(url, { json: true }, (error, response, body) => {
         }
       }
     }
-    console.log(completedByUser);
+    let count = 0;
+    process.stdout.write("{")
+    for (const key in completedByUser) {
+        if (count != 0) {
+            process.stdout.write(",\n")
+            process.stdout.write(" ")
+        } else {
+        }
+        count += 1;
+        const value = completedByUser[key];
+        process.stdout.write(` \u001b[32m'${key}'\u001b[0m: \u001b[33m${value}\u001b[0m`);
+      }
+    console.log(" }")
   } else {
     console.error(error, 'hello');
   }
