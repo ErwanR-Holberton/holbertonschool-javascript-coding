b31098a49cfc5462ea2c074503028f0e31bdacb4
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-31 23:11:03 +0100

    Corrected output

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,11 +3,19 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    let students = data.split('\n');
-    students = students.filter((line) => line.trim() !== '');
-    console.log(`Number of students: ${students.length}`);
+    const countByField = { CS: [], SWE: [] };
+    const students = data.split('\n');
+    students.filter((line) => line.trim() !== '')
+      .forEach((item) => {
+        const values = item.split(',');
+        const field = values[3].trim();
+        if (field === 'CS' || field === 'SWE') { countByField[field].push(values[0]); }
+      });
+    console.log(`Number of students: ${countByField.SWE.length + countByField.CS.length}`);
+    console.log(`Number of students in CS: ${countByField.CS.length}. List: ${countByField.CS.join(', ')}`);
+    console.log(`Number of students in SWE: ${countByField.SWE.length}. List: ${countByField.SWE.join(', ')}`);
   } catch (err) {
-    console.log('Cannot load the database');
+    console.log('Cannot load the database', err);
   }
 }
 module.exports = countStudents;
