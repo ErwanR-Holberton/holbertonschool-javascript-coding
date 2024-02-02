563c3ca662bc8ee05432dee5b30b7991e60101a7
ErwanR-Holberton <6218@holbertonstudents.com>
2024-02-02 14:01:08 +0100

    Push task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) { reject(new Error('Cannot load the database')); return; }
+
+      const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
+      const header = lines[0].split(',');
+
+      const students = lines.slice(1).map((line) => {
+        const values = line.split(',');
+        return {
+          firstname: values[header.indexOf('firstname')],
+          field: values[header.indexOf('field')],
+        };
+      });
+
+      const fieldCounts = {};
+      students.forEach((student) => {
+        if (fieldCounts[student.field] === undefined) {
+          fieldCounts[student.field] = 1;
+        } else {
+          fieldCounts[student.field] += 1;
+        }
+      });
+
+      console.log('Number of students:', students.length);
+      Object.keys(fieldCounts).forEach((field) => {
+        console.log(`Number of students in ${field}: ${fieldCounts[field]}. List: ${students.filter((student) => student.field === field).map((student) => student.firstname).join(', ')}`);
+      });
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
