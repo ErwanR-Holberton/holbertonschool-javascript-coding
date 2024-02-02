cc947b3c088504035799a405bac923fbe1b1676a
ErwanR-Holberton <6218@holbertonstudents.com>
2024-02-02 15:17:59 +0100

    Push task 5

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer(async (req, res) => {
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello Holberton School!');
+    } else if (req.url === '/students') {
+		countStudents('./database.csv')
+        .then((result) => {
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(`This is the list of our students\n${result}`);
+        })
+        .catch(error => {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end(`Error: ${error.message}\n`);
+        });
+    }
+  }
+});
+
+const PORT = 1245;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
