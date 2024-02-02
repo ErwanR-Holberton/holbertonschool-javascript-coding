11d5e066129fb21187eac33de143c00e98952881
ErwanR-Holberton <6218@holbertonstudents.com>
2024-02-02 22:47:26 +0100

    Test new version

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,7 @@
-const readline = require('readline');
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-const interfaceIsReserved = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-console.log('Welcome to Holberton School, what is your name?');
-
-interfaceIsReserved.question('', (input) => {
-  console.log('Your name is:', input);
-  interfaceIsReserved.close();
-});
-
-interfaceIsReserved.on('close', () => {
-  console.log('This important software is now closing');
-  process.exit(0);
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
+  process.stdout.write("This important software is now closing\n");
+  process.exit();
 });
