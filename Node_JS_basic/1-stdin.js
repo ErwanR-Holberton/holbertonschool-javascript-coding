9e3080fd44669a6278f6d4b6b94e822b61b3e02a
ErwanR-Holberton <6218@holbertonstudents.com>
2024-02-02 22:49:13 +0100

    Added require main

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,9 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+if (require.main === module) {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString()}`);
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
-});
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  });
+}
