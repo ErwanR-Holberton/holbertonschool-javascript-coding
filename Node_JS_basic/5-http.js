8d76336d675f773f36c06ae8a9b45ce9f367a76e
ErwanR-Holberton <6218@holbertonstudents.com>
2024-02-04 13:13:13 +0100

    Use argv 2 as database

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,13 +1,15 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
+const path = process.argv[2];
+
 const app = http.createServer(async (req, res) => {
   if (req.method === 'GET') {
     if (req.url === '/') {
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end('Hello Holberton School!');
     } else if (req.url === '/students') {
-      countStudents('./database.csv')
+      countStudents(path)
         .then((result) => {
           res.writeHead(200, { 'Content-Type': 'text/plain' });
           res.end(`This is the list of our students\n${result}`);
